3c959baad55ec0f0cf825deb3e5feb9a58b9320a
Bryant Wang and Hal Fairless <bryant@>
2016-11-02 15:51:19 -0700

    Add script to check if someone wins.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var arrayOfPositions = [];
-var flatArrayOfPositions = [];
+var positions = [];
+var flatPositions = [];
 
-// Constructor for each location in a game board
+// Constructor for each location in a game board.
 function Position() {
   this.occupied = false;
-  this.mark = "";
+  this.mark = "-";
   this.name = "";
 }
 // Constructor for players
@@ -12,21 +12,66 @@ function Player(mark) {
   this.mark = mark;
   this.turn = false;
 }
-// Game board creator
+// Fills flatPositions, intended for use by boardCreator.
 function positionCreator() {
   for(idx = 0; idx < 9; idx++) {
-    flatArrayOfPositions[idx] = new Position();
-    flatArrayOfPositions[idx].name = "position-" + (idx + 1);
+    flatPositions[idx] = new Position();
+    flatPositions[idx].name = "position-" + (idx + 1);
   }
 }
-
+// Creates a 3 by 3 board filled with Position objects.
 function boardCreator() {
   for(idx = 0; idx < 3 ; idx++) {
-    arrayOfPositions[idx] = [];
+    positions[idx] = [];
     for(idx2 = 0; idx2 < 3; idx2++) {
-      arrayOfPositions[idx].push(flatArrayOfPositions[0]);
-      flatArrayOfPositions.shift();
+      positions[idx].push(flatPositions[0]);
+      flatPositions.shift();
     }
+  }
+}
+// Checks if someone has won.
+function winChecker() {
+  // Tests horizontal winning possibilities.
+  for(idx = 0; idx < 3; idx++) {
+    var rowTest = "" + positions[idx][0].mark + positions[idx][1].mark + positions[idx][2].mark;
+
+    if(rowTest === "XXX" || rowTest ==="OOO") {
+      alert("Winner!");
+    }
+  }
+  // Tests vertical winning possibilities.
+  for(idx = 0; idx < 3; idx++) {
+    var columnTest = "" + positions[0][idx].mark + positions[1][idx].mark + positions[2][idx].mark;
+
+    if(columnTest === "XXX" || columnTest === "OOO") {
+      alert("Winner!");
+    }
+  }
+
+  // Tests diagonal winning possibilities.
+  var crossTest = "" + positions[0][0].mark + positions[2][2].mark + positions[1][1].mark + positions[0][2].mark + positions[2][0].mark;
+
+  if(crossTest.includes("XXX") || crossTest.includes("OOO")) {
+    var testIndexX = crossTest.lastIndexOf("XXX");
+    var testIndexO = crossTest.lastIndexOf("OOO");
+
+    if(testIndexX === 1 || testIndexO === 1) {
+    } else {
+      alert("Winner!");
+    }
+  }
+  // Counts to check how many spaces are occupied, and tells the players that it's a draw once it reaches 9 and no player has been determined.
+  var occupationCounter = 0;
+  for(idx = 0; idx < 3; idx++) {
+    for(idx2 = 0; idx2 < 3; idx2++) {
+      if(positions[idx][idx2].occupied) {
+        occupationCounter++;
+      }
+    }
+  }
+
+  if(occupationCounter >= 9) {
+    alert("Draw!");
   }
 }
 
@@ -36,7 +81,6 @@ $(function() {
   var player1 = new Player("X");
   var player2 = new Player("O");
   player1.turn = true;
-  console.log(player1.turn + ", " + player2.turn);
 
   function turnSwitcher() {
     if(player1.turn) {
@@ -55,17 +99,18 @@ $(function() {
   $(".col-borders").click(function() {
     for(idx = 0; idx < 3; idx++) {
       for(idx2 = 0; idx2 < 3; idx2++) {
-        if(this.id === arrayOfPositions[idx][idx2].name) {
-          if(arrayOfPositions[idx][idx2].occupied === false) {
+        if(this.id === positions[idx][idx2].name) {
+          if(positions[idx][idx2].occupied === false) {
             if(player1.turn) {
-              arrayOfPositions[idx][idx2].mark = player1.mark;
+              positions[idx][idx2].mark = player1.mark;
               this.append(player1.mark);
             } else {
-              arrayOfPositions[idx][idx2].mark = player2.mark;
+              positions[idx][idx2].mark = player2.mark;
               this.append(player2.mark);
             }
-            arrayOfPositions[idx][idx2].occupied = true;
+            positions[idx][idx2].occupied = true;
             turnSwitcher();
+            winChecker();
           }
         }
       }
